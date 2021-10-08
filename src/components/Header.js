6f1598862143b5cdbd391e4d25ae7e44173f17ef
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-10-09 01:37:12 +0300

    Nav links changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import headerImage from '../images/header-img.PNG';
 import cameraBackground from '../images/camera-background.jpg';
 
@@ -6,13 +6,17 @@ export const Header = () => {
 
     const navStyles = {
         show: {display: "flex"},
-        hide: {display: "none"}
+        hide: {display: "none"},
+        filled: { backgroundColor: "rgb(20, 20, 20)"},
+        unfilled: { backgroundColor: "transparent" }
     }
+
     const navButtonStyles = {
         down: { margin: "rotateX(0deg)"},
         up: { transform: "rotateX(180deg)"}
     }
 
+    const [navColor, setNavColor] = useState(navStyles.unfilled);
     const [showNav, setShowNav] = useState(true);
     const [rotateNavButton, setRotateNavButton] = useState(false);
 
@@ -21,16 +25,50 @@ export const Header = () => {
         setRotateNavButton(!rotateNavButton);
     }
 
+    useEffect(() => {
+        //Handle nav bar color on scroll event 
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY < 200) {
+            setNavColor(navStyles.unfilled)
+        } else {
+            setNavColor(navStyles.filled)
+        }
+    } 
+
+    const scrollToViewWork = () => {
+        const position = getScrollPosition(".View-Work");
+        window.scrollTo(0, position);
+    }
+
+    const scrollToAboutMe = () => {
+        const position = getScrollPosition(".About-Me");
+        window.scrollTo(0, position);
+    }
+
+    const scrollToContactMe = () => {
+        const position = getScrollPosition(".Contact-Me");
+        window.scrollTo(0, position);
+    }
+
+    const getScrollPosition = (element) => {
+        const position = parseInt(document.querySelector(element).offsetTop);
+        const navHeight = parseInt(document.querySelector(".container nav").offsetHeight);
+        return position - navHeight;
+    }
+
     return (
         <div className= "Header" id= "Header">
             <img className="camera" src={cameraBackground} alt="Camera background" />
             <div className="container">
-                <nav>
+                <nav style={navColor}>
                     <h1><a href="#Header">Mohammad <span>|</span> Haj Ali</a></h1>
                     <ul style={showNav? navStyles.show : navStyles.hide}>
-                        <li><p className="view-work-btn">View Work</p></li>
-                        <li><p className="about-me-btn">About Me</p></li>
-                        <li><p className="contact-me-btn">Contact Me</p></li>
+                        <li><p onClick={scrollToViewWork}>View Work</p></li>
+                        <li><p onClick={scrollToAboutMe}>About Me</p></li>
+                        <li><p onClick={scrollToContactMe}>Contact Me</p></li>
                     </ul>
                     <i className="fa fa-caret-down menu-btn" onClick={navButtonClick} style={rotateNavButton? navButtonStyles.up : navButtonStyles.down}/>
                 </nav>
